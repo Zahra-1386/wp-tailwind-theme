a35f5c13c7a1beac5c6581329e2801420512fd47
Zahra-1386 <zahra86hamidi@gmail.com>
2024-12-14 16:46:30 -0800

    Implemented the homepage

--- index.php
+++ index.php
@@ -1,15 +1,55 @@
 <?php
 
 get_header(); ?>
-<div class="max-w-screen-lg">
-    <div class="grid grid-cols-1 md:grid-cols-3">
-        <?php while (have_posts()) : the_post();        ?>
-        <a href="<?=get_the_permalink()?>">
-            <?php the_title();?>
+<img src="<?= get_template_directory_uri() ?>/assets/img/slider.jpg" alt="">
+<?php
+$categories = get_categories(["hide_empty" => 0]);
+?>
+<div class="max-w-screen-x1 flex flex-wrap">
+    <?php
+    foreach ($categories as $categoriy) {
+        if (function_exists('z_taxonomy_image')) {
+            echo '<div>';
+            z_taxonomy_image($categoriy->term_id);
+            echo '</div>';
+        }
+    }
+    ?>
+</div>
+<div class="max-w-screen-lg mx-auto">
+    <h2 class="product-title text-center dual-color mb-10 mt-6">
+        <span>آخرین</span>
+        محصولات
+    </h2>
+</div>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+    <?php while (have_posts()) : the_post();
+        $price = get_post_meta(get_the_ID(), 'price', true);
+        $finalprice = get_post_meta(get_the_ID(), 'final_price', true);
+        $cat = get_the_category();
+    ?>
+        <a href="<?= get_the_permalink() ?>" class="post-box relative">
+            <span class="cat"><?= $cat[0]->name ?></span>
+            <?php
+            the_post_thumbnail(); ?>
+            <span class="title">
+                <?php the_title(); ?>
+            </span>
+            <?php if (!empty($price)): ?>
+                <span class="mx-auto price block w-fit">
+                    <?= (toFarsiNumerals(number_format($price))) ?>
+                </span>
+            <?php endif; ?>
+            <?php if (!empty($finalprice)): ?>
+                <span class="mx-auto final-price block w-fit text-orange">
+                    <?= (toFarsiNumerals(number_format($finalprice))) ?>
+                </span>
+            <?php endif; ?>
         </a>
-        <?php endwhile; ?>
-    </div>
-
+    <?php endwhile; ?>
+</div>
+<div class="py-5">
+    <?php the_posts_pagination() ?>
 </div>
 
 
